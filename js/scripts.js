a14c0f88fe0a81daa313149d54eacb0c4e053c04
jacey tupa and kohichi horii <kohichi@>
2018-10-16 16:43:30 -0700

    Fix entire code.

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,33 @@
 $(document).ready(function() {
-  $("li").click(function() {
-    $(".hideclass#Term1Def").toggle();
+  $('#Term1').click(function(){
+    $('.definitions li').removeClass();
+    $('#Term1Def').addClass("show");
   });
+
+  $('#Term2').click(function(){
+    $('.definitions li').removeClass();
+    $('#Term2Def').addClass("show");
+  });
+
+
+
+
+  // $("#Term1").click(function() {
+  //   console.log('ckiks');
+  //   $(".definitions li").removeClass();
+  //   $("#Term1Def").addClass(".showclass");
+  //   $(".hideclass#Term1Def").toggle();
+  // });
+  // $("#Term2").click(function() {
+  //   $("li.definitions").removeClass();
+  //   $("Term2Def").addClass(".showclass");
+  //   // $(".hideclass#Term2Def").toggle();
+  // });
+
+
+
+  // $(".showclass#Term2").click(function() {
+  //   $("body").removeClass();
+  //   $(".hideclass#Term2Def").toggle();
+  // });
 });
